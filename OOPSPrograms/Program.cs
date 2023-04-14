760992c518e1966074c37289566feb503b52c989
Shaik Basha Vali <bashavali.shaik@outlook.com>
2023-04-15 02:29:09 +0530

    [Basha] InventoryDataManagement

---
+++ OOPSPrograms/Program.cs
@@ -0,0 +1,15 @@
+﻿using OOPSPrograms.InventoryDataManagement;
+using System;
+namespace OOPSPrograms
+{
+    class Program
+    {
+        static string jsonFilePath = @"C:\Users\Basha\Documents\BridgeLabz\Object_Oriented_Programs\OOPSPrograms\InventoryDataManagement\Inventory.json";
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the OOPS Programs");
+            InventoryDetailManagement inventory = new InventoryDetailManagement();
+            inventory.ReadJSONFile(jsonFilePath);
+        }
+    }
+}
